9397b82cc0693968c1c17236ede2e1119e01ad8b
StanislavKhartov <89999222+StanislavKhartov@users.noreply.github.com>
2021-09-14 13:26:53 +0300

    add solution

---
+++ aequence/sequence/sequence.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "PLease, enter amount of sequence\n";
+    int n;
+    cin >> n;
+    int first = 0;
+    int second = 0;
+    int third = 0;
+    int fourth = 0;
+    int answer = 0;
+    int current = 0;
+    cout << "Please, enter members of sequence\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> fourth;
+        current = current - first + fourth;
+        answer = max(answer, current);
+        first = second;
+        second = third;
+        third = fourth;
+    }
+    cout << answer;
+}
